5f8533a770a9601cd522f17f297ad299f222f2c5
澤井聖也 <softtennis.s.s@icloud.com>
2016-03-14 10:26:43 +0900

    Initial Commit

---
+++ Arduino_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Arduino_ios
+//
+//  Created by 澤井聖也 on 2016/03/14.
+//  Copyright © 2016年 澤井聖也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Arduino_iosUITests/Arduino_iosUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Arduino_iosUITests.swift
+//  Arduino_iosUITests
+//
+//  Created by 澤井聖也 on 2016/03/14.
+//  Copyright © 2016年 澤井聖也. All rights reserved.
+//
+
+import XCTest
+
+class Arduino_iosUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
